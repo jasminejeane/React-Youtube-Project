23f383f76a4c5ec995a45a08c47b7a753633b558
jasminejeane <jasminejeane@gmail.com>
2016-11-01 02:56:28 -0700

    created markup and added video

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBRu4OUWShMCkYiuN-LR3HRKj5jdzhcUm8';
 
@@ -24,6 +25,7 @@ class App extends Component{
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
 
